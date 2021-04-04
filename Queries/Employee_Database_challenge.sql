f7bb74c5b9cecbb5bf5b521c7bcda0a2dfdac211
Onejohnson Ekwueme <you@example.com>
2021-04-04 13:13:44 -0400

    Challenge complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -27,3 +27,23 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Creating membership eligibility table
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ts.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ts
+	ON (e.emp_no = ts.emp_no)
+WHERE ((e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND de.to_date = '9999-01-01')
+ORDER BY e.emp_no, ts.from_date DESC;
+
+
+
